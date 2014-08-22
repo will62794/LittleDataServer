0e800a1b420290a8bec9ad4e071796910c29a58b
William Schultz <will627@comcast.net>
2014-08-22 13:28:13 -0400

    added Value color option

--- Renderers/ColorBarRenderer.py
+++ Renderers/ColorBarRenderer.py
@@ -9,7 +9,7 @@ class ColorBarRenderer(Renderer):
 		self.description="The MagnitudeColorBar Renderer renderer takes a key and a value, a Colorscale, and a max and min value for the data values and draws a bar of a certain color and height."
 		self.colorscale=[[139,68,254],[255,10,50],[155,20,50]]
 		self.options={"RandomizeColors":False}
-		self.coloroptions={"TextColor":[0,0,0],"BackgroundColor":[0,152,140]}
+		self.coloroptions={"TextColor":[0,0,0],"BackgroundColor":[0,152,140],"ValueColor":[0,0,0]}
 
 	def draw(self,keyValuePair):
 		key=keyValuePair[0]
@@ -29,7 +29,7 @@ class ColorBarRenderer(Renderer):
 
 		self.drawer.drawRectangle(0,0,16,64,backgroundcolor)
 
-		self.drawer.drawCenteredValue(str(value)[:2],50,[0,0,0],"8x13")
+		self.drawer.drawCenteredValue(str(value)[:2],50,self.coloroptions["ValueColor"],"8x13")
 		
 		self.drawer.drawVerticalString(2,key.strip().upper()[:6],textcolor,"6x10")
 		self.drawer.transition("slide")
