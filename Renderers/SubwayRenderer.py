5c8850059acb12fc985a085d401b78c2a44f1fa5
William Schultz <will627@comcast.net>
2014-08-22 16:10:15 -0400

    m

--- Renderers/SubwayRenderer.py
+++ Renderers/SubwayRenderer.py
@@ -28,15 +28,15 @@ class SubwayRenderer(Renderer):
 		if value>10:
 			value=10
 
-		pixelheight=int(value*4.2)
+		pixelheight=42-int(value*4.2)
 
-		self.getColorMix([255,255,255],[255,0,0],1.0-float(value)/10)
+		c=self.getColorMix([255,255,255],[255,0,0],1.0-float(value)/10)
 		
 		self.drawer.barNum=0
 		self.drawer.clearDrawBuffer()
 		self.drawer.drawCircle(7,55,7,routecolor)
-		self.drawer.drawRectangle(0,0,16,pixelheight,[250,250,250])
-		self.drawer.drawTriangle(0,pixelheight,8,pixelheight+5,16,pixelheight,[250,250,250])
+		self.drawer.drawRectangle(0,0,16,pixelheight,c)
+		self.drawer.drawTriangle(0,pixelheight,8,pixelheight+5,16,pixelheight,c)
 
 		self.drawer.drawChar(5,51,[0,0,0],"6x10",routename)
 		self.drawer.bufferswap("false")
